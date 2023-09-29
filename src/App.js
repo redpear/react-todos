a9cc293292400f8649f560ed523dc13ef2102f30
Angie <redpear@mac.com>
2023-09-29 16:46:12 -0700

    React frontend todos app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button, Space } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
